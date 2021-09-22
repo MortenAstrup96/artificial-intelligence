386e7422c33af94ed401c0e2b49ce08ba9820ec8
Martin Kjær <martinkjaer@uu-guest-145-22.eduroam.uu.se>
2021-09-22 10:44:20 +0200

    initialize work

--- assignment-2/croc.R
+++ assignment-2/croc.R
@@ -1,3 +1,20 @@
 myFunction = function(moveInfo, readings, positions, edges, probs) {
     print("test")
+  
+  #Initial state probability for each node
+  #prob_vector <- moveInfo$mem
+  
+  if(moveInfo$mem$status == 0) {prob_vector <- initialize(positions)}
+  print(prob_vector)
+  
 }
+
+#If first round set all prob except backpacker location
+initialize = function(pos) {
+  prob_vector <- rep(1/38,40)
+  prob_vector[pos[1]] <- 0
+  prob_vector[pos[2]] <- 0
+  return (prob_vector)
+}
+
+
